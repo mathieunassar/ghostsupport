6891eafd1b4d27e6af15fcb8a800d8bf42134bf8
MathieuNassar <mathieu.nassar@gmail.com>
2020-08-20 18:13:03 +0200

    ADD: *getAndPop

--- include/BlockingQueue.impl.hpp
+++ include/BlockingQueue.impl.hpp
@@ -14,13 +14,13 @@
  * limitations under the License.
  */
 
-/// returns a reference to the next element of the queue, but does not remove it. If the queue is empty, this call is blocking.
+ /// returns a reference to the next element of the queue, but does not remove it. If the queue is empty, this call is blocking.
 template <typename T>
 const T& BlockingQueue<T>::get() const
 {
 	std::unique_lock<std::mutex> lock(_mutex);
 	_condition.wait(lock, [=] { return !_queue.empty(); });
-	
+
 	return _queue.back();
 }
 
@@ -36,6 +36,32 @@ bool BlockingQueue<T>::tryGet(std::chrono::milliseconds timeout, T& result)
 		return false;
 
 	result = _queue.back();
+	return true;
+}
+
+template <typename T>
+T BlockingQueue<T>::getAndPop() const
+{
+	std::unique_lock<std::mutex> lock(_mutex);
+	_condition.wait(lock, [=] { return !_queue.empty(); });
+
+	T res = _queue.back();
+	_queue.pop_back();
+	return std::move(res);
+}
+
+template <typename T>
+bool BlockingQueue<T>::tryGetAndPop(std::chrono::milliseconds timeout, T& result) const
+{
+	std::unique_lock<std::mutex> lock(_mutex);
+	if (_queue.size() == 0)
+		_condition.wait_for(lock, timeout, [=] { return !_queue.empty(); });
+
+	if (_queue.size() == 0)
+		return false;
+
+	result = std::move(_queue.back());
+	_queue.pop_back();
 	return true;
 }
 
